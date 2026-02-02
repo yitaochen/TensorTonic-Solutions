206c11b17f7c37696a66300b43cd9bbfb70f170f
Yitao Chen <yitaochen@utexas.edu>
2026-02-01 16:36:19 -0800

    Update solution for linear-layer-forward

--- linear-layer-forward/linear-layer-forward.py
+++ linear-layer-forward/linear-layer-forward.py
@@ -1,12 +1,27 @@
-import numpy as np 
+
 
 def linear_layer_forward(X, W, b):
     """
     Compute the forward pass of a linear (fully connected) layer.
     """
     # Write code here
-    X = np.asarray(X)
-    W = np.asarray(W)
-    b = np.asarray(b)
+    m, n = len(X), len(W[0])
+    Y = [[0 for _ in range(n)] for _ in range(m)]
 
-    return (X @ W + b).tolist()
+    def dot_product(a, b):
+        if len(a) != len(b):
+            raise ValueError("Lists must have the same length")
+        ans = 0
+        for i in range(len(a)):
+            ans += a[i] * b[i]
+        return ans 
+    
+    W_cols = list(zip(*W))
+    
+    # print(W, W_cols)
+    # print(m, n)
+    for i in range(m):
+        for j in range(n):
+            Y[i][j] = dot_product(X[i], W_cols[j]) + b[j] 
+    # print(X, W, b, Y)
+    return Y
