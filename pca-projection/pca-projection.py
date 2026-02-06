8e95d764132ffd649fc41a375a42a376a744692b
Yitao Chen <yitaochen@utexas.edu>
2026-02-05 21:15:44 -0800

    Add solution for pca-projection

---
+++ pca-projection/pca-projection.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def pca_projection(X, k):
+    """
+    Project data onto the top-k principal components.
+    """
+    # Write code here
+    X = np.asarray(X)
+    X_centered = X - np.mean(X, axis=0, keepdims=True)
+    n, d = X_centered.shape
+    # print(f"n: {n} d: {d}")
+    C = X_centered.T @ X_centered / (n - 1)
+
+    W = []
+
+    for i in range(k):
+        v = np.random.rand(d)
+        v = v / np.linalg.norm(v)
+        for _ in range(100):
+            v_new = C @ v 
+            v = v_new / np.linalg.norm(v_new)
+        W.append(v)
+        eigen_val = (v.T @ C) @ v
+        C -= eigen_val * np.outer(v, v)
+
+    W = np.hstack(W).reshape((d, k))
+    # print(W.shape)
+    ans = (X_centered @ W)
+
+    return ans.tolist()
+
