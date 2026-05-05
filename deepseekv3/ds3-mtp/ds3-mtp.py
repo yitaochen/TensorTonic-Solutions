eb032d6724f4944c9977fc70bac0d5071c1ca282
Yitao Chen <yitaochen@utexas.edu>
2026-05-05 15:53:49 -0700

    Update solution for ds3-mtp

--- deepseekv3/ds3-mtp/ds3-mtp.py
+++ deepseekv3/ds3-mtp/ds3-mtp.py
@@ -5,8 +5,7 @@ def mtp_head(h: torch.Tensor, W_projs: list, W_head: torch.Tensor) -> torch.Tens
     Returns: torch.Tensor of shape (batch, seq_len, num_predict, vocab_size)
     """
     # YOUR CODE HERE
-    D = len(W_projs)
-    hd = torch.stack([h @ W_projs[i].T for i in range(D)], dim=-2)
+    hd = torch.stack([h @ W_proj.T for W_proj in W_projs], dim=-2)
     logits = hd @ W_head.T
 
     return logits
